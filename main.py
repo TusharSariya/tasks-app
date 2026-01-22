a949d127f0a3431610a73dffb10fd116bb044e77
Tushar Sariya <tushar.97@hotmail.com>
2026-01-22 10:13:00 -0330

    make some entities

--- main.py
+++ main.py
@@ -11,28 +11,41 @@ class Author(db.Model):
     name = db.Column(db.String(30), nullable=False)
     age = db.Column(db.Integer,nullable=False)
     height = db.Column(db.Float,nullable=False)
-    peers        = db.Column(db.JSON,default=list)
 
     # Self-referential relationship
     boss_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=True)
-    subordinates = db.relationship('Author', backref=db.backref('boss', remote_side=[id]))
+    subordinates = db.relationship('Author', backref=db.backref('boss', remote_side=[id])) #this is magically a list because of how sql alchemy works
 
     tasks = db.relationship('Task', backref='author', lazy=True)
 
+    @property
+    def peers(self):
+        """Returns a list of other Authors who share the same boss."""
+        if self.boss:
+            return [x for x in self.boss.subordinates if x.id != self.id]
+        return []
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    headline = db.Column(db.string(200), nullable=False)
+    headline = db.Column(db.String(200), nullable=False)
     content = db.Column(db.String(1000), nullable=False)
-    date    = db.Column(db.dateTime,default=None,nullable=True)
+    date    = db.Column(db.DateTime,default=None,nullable=True)
     author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
-    creation_date = db.Column(db.dateTime,default=datetime.now(timezone.utc))
+    creation_date = db.Column(db.DateTime,default=lambda: datetime.now(timezone.utc))
 
     def __repr__(self):
         return f'<Task {self.id}>'
 
 with app.app_context():
+    db.drop_all()
     db.create_all()
-    dentist = Task(content="dentist")
+    
+    jones = Author(name="Jonny Jones", age=25, height=1.8)
+    emily = Author(name="Emily Hynes", age=30, height=1.0, boss=jones)
+    due_date = datetime(2026, 1, 7, 9, 0)
+    meeting_prep = Task(headline="meeting prep", content="lorem ipsum how the buisness makes money", date=due_date, author=emily)
+    db.session.add_all([jones, emily, meeting_prep])
+    db.session.commit()
 
 @app.route('/')
 def index():
@@ -41,9 +54,9 @@ def index():
 
 @app.route("/viewtasks")
 def viewtasks():
-    tasks = Task.query(Task.headline,Author.name).join(Author).all()
+    tasks = db.session.query(Task.headline, Author.name).join(Author).all()
     print(tasks)
-    return tasks
+    return str(tasks)
 
 if __name__ == '__main__':
     app.run(debug=True)
